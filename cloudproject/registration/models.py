88912fb077a67b21d305850c9fe9956ac6b026b5
66070224 <66070224@kmitl.ac.th>
2025-09-08 00:11:22 +0700

    หน้า register เก็อบเสร็จ

--- cloudproject/registration/models.py
+++ cloudproject/registration/models.py
@@ -5,17 +5,18 @@ from django.contrib.auth.models import User as AuthUser
 # User
 #----------------------------------------------
 class UserInfo(models.Model):
-    user = models.OneToOneField(AuthUser, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(AuthUser, on_delete=models.CASCADE, null=True, blank=True)
+    email = models.EmailField(unique=True)
     title = models.CharField(max_length=10)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     class RoleChoices(models.TextChoices):
         STUDENT = 'STU', 'Student'
         TEACHER = 'TEA', 'Teacher'
-    role = models.CharField(max_length=3, choices=RoleChoices.choices, null=True)
+    role = models.CharField(max_length=3, choices=RoleChoices.choices)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name} ({self.role})"
 
 #----------------------------------------------
 # Faculty and Department
@@ -27,7 +28,7 @@ class Faculty(models.Model):
         return self.name
 
 class Department(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name='departments')
 
     def __str__(self):
@@ -59,10 +60,11 @@ class Teacher(models.Model):
 # Course and Section
 #----------------------------------------------
 class Course(models.Model):
-    code = models.CharField(max_length=10, primary_key=True)
-    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100, unique=True)
     credits = models.IntegerField()
     department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='courses')
+    teachers = models.ManyToManyField(Teacher, related_name="courses")
 
     def __str__(self):
         return f"{self.code} - {self.name} ({self.department.name})"
@@ -70,41 +72,46 @@ class Course(models.Model):
 class CourseSection(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_sections')
     section_number = models.IntegerField()
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    capacity = models.IntegerField()
+    class Meta:
+        unique_together = ('course', 'section_number')
+
+    def __str__(self):
+        return f"{self.course.code} {self.course.name} - Section {self.section_number}"
+    
+class SectionClass(models.Model):
+    section = models.ForeignKey(CourseSection, on_delete=models.CASCADE, related_name='section_class')
     class TypeChoices(models.TextChoices):
         LECTURE = 'LEC', 'Lecture'
         LAB = 'LAB', 'Lab'
     class_type = models.CharField(max_length=3, choices=TypeChoices.choices)
     class DayChoices(models.TextChoices):
-        MONDAY = 'Mon', 'Monday'
-        TUESDAY = 'Tue', 'Tuesday'
-        WEDNESDAY = 'Wed', 'Wednesday'
-        THURSDAY = 'Thu', 'Thursday'
-        FRIDAY = 'Fri', 'Friday'
-        SATURDAY = 'Sat', 'Saturday'
-        SUNDAY = 'Sun', 'Sunday'
+        SUNDAY = 'Sun', 'อาทิตย์'
+        MONDAY = 'Mon', 'จันทร์'
+        TUESDAY = 'Tue', 'อังคาร'
+        WEDNESDAY = 'Wed', 'พุทธ'
+        THURSDAY = 'Thu', 'พฤหัสบดี'
+        FRIDAY = 'Fri', 'ศุกย์'
+        SATURDAY = 'Sat', 'เสาร์'
     day = models.CharField(max_length=3, choices=DayChoices.choices)
     start_time = models.TimeField()
     end_time = models.TimeField()
     location = models.CharField(max_length=100)
-    capacity = models.IntegerField()
-
-    class Meta:
-        unique_together = ('course', 'section_number')
 
     def __str__(self):
-        return f"{self.course.code} - Section {self.section_number}"
+        return f"{self.section.course.code} - Section {self.section.section_number}: {self.day} {self.start_time}-{self.end_time}"
 
 
 #----------------------------------------------
 # Enroll
 #----------------------------------------------
-class PreEnrollQueue(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    course_section = models.ForeignKey(CourseSection, on_delete=models.CASCADE)
+class EnrollQueue(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='enroll_queues')
+    course_section = models.ForeignKey(CourseSection, on_delete=models.CASCADE, related_name='enroll_queues')
+    enroll_time = models.DateTimeField(auto_created=True)
 
     class Meta:
         unique_together = ('student', 'course_section')
     
     def __str__(self):
-        return f"{self.student.code} - {self.course_section.course.code} ({self.course_section.section_number})"
+        return f"{self.student.code} - {self.course_section.course.name}:{self.course_section.section_number} - {self.enroll_time}"
--- cloudproject/registration/views.py
+++ cloudproject/registration/views.py
@@ -26,22 +26,34 @@ class RegistrationView(LoginRequiredMixin, View):
                 "code": course.code,
                 "name": course.name,
                 "credits": course.credits,
+                "teachers": [
+                    {
+                        "title": teacher.from_user.title,
+                        "first_name": teacher.from_user.first_name,
+                        "last_name": teacher.from_user.last_name,
+                    }
+                    for teacher in course.teachers.all()
+                ],
                 "sections": [
                     {
-                        "id": section.id,
                         "section_number": section.section_number,
-                        "teacher": section.teacher.from_user.title + section.teacher.from_user.first_name + " " + section.teacher.from_user.last_name,
-                        "day": section.day,
-                        "start_time": section.start_time,
-                        "end_time": section.end_time,
-                        "location": section.location,
                         "capacity": section.capacity,
                         "enrolled_count": section.enrolled_students.count(),
+                        "section_class": [
+                            {
+                                "day": section_class.get_day_display(),
+                                "start_time": section_class.start_time,
+                                "end_time": section_class.end_time,
+                                "location": section_class.location,
+                            }
+                            for section_class in section.section_class.all()
+                        ]
                     }
                     for section in course.course_sections.all()
                 ]
             })
-        context = {"courses": data}
+        days = ["อาทิตย์", "จันทร์", "อังคาร", "พุธ", "พฤหัสบดี", "ศุกย์", "เสาร์"]
+        context = {"courses": data, "days": days, "times": list(range(12))}
         return render(request, "registration.html", context)
     def post(self, request):
         
