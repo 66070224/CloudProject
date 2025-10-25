e0c6d2c6a0cf121626827b11eb27d00a5c57d868
Takku1N <lightroj00@gmail.com>
2025-10-25 21:12:55 +0700

    init

--- CloudProject/settings.py
+++ CloudProject/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'CloudProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'register',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres_2547',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'register',
-    #     'USER': 'regist_it21',
-    #     'PASSWORD': 'postgres_password_regist_it21',
-    #     'HOST': 'db-regist-66070317.cpikuy4u2d65.us-east-1.rds.amazonaws.com',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres_2547',
+    #     'HOST': 'localhost',
     #     'PORT': '5432',
     # }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'register',
+            'USER': 'root',
+            'PASSWORD': 'Cloud_rds_1234',
+            'HOST': 'cloud-project-rds.cpikuy4u2d65.us-east-1.rds.amazonaws.com',
+            'PORT': '5432',
+        }
 }
 
 
@@ -154,6 +154,10 @@ AWS_S3_REGION_NAME = "us-east-1"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # # Media files
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
 # AWS Cognito setting
 COGNITO_USER_POOL_ID = "us-east-1_8tddVcR3w"
