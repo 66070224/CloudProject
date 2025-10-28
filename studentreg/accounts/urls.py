2159ec2724d0657360ccea7ecf03028a8eebc19c
Takku1N <lightroj00@gmail.com>
2025-10-28 16:04:42 +0700

    S3...

--- studentreg/accounts/urls.py
+++ studentreg/accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from accounts.views import LoginView, LogoutView, ProfileView, ChangePassword
 
 urlpatterns = [
-    path('login', LoginView.as_view(), name="login"),
-    path('logout', LogoutView.as_view(), name="logout"),
-    path('profile', ProfileView.as_view(), name="profile"),
-    path('changepassword', ChangePassword.as_view(), name="changepassword"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('profile/', ProfileView.as_view(), name="profile"),
+    path('changepassword/', ChangePassword.as_view(), name="changepassword"),
 ]
--- studentreg/studentreg/settings.py
+++ studentreg/studentreg/settings.py
@@ -149,3 +149,16 @@ NPM_BIN_PATH = 'npm.cmd'
 
 if DEBUG:
     INSTALLED_APPS += ['django_browser_reload']
+
+# AWS S3 setting
+INSTALLED_APPS += ['storages']
+
+AWS_ACCESS_KEY_ID = config('ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = config('S3_REGION_NAME')
+
+# # Static files
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# # Media files
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
