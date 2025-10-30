97df6b2c7113f1f5df7d4c6ee5968af878bd9d8d
66070224 <66070224@kmitl.ac.th>
2025-10-30 17:34:37 +0700

    API ลบ คน

--- studentreg/personnels/views.py
+++ studentreg/personnels/views.py
@@ -8,6 +8,8 @@ from accounts.models import CustomUser
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from rest_framework.views import APIView
+
 #----------------------------------------------------------------------------------------------------------------------------
 # STUDENT
 #----------------------------------------------------------------------------------------------------------------------------
@@ -346,3 +348,63 @@ class PaymentDetailView(LoginRequiredMixin, View):
         payment.status = "Y"
         payment.save()
         return redirect(reverse("personel_payment_list"))
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+# API
+#----------------------------------------------------------------------------------------------------------------------------
+
+class DeleteRegistraAPI(LoginRequiredMixin, APIView):
+    def get(self, request, id):
+        
+        if not request.user.is_admin:
+            return redirect(reverse("home"))
+
+        try:
+            with transaction.atomic():
+                user = CustomUser.objects.get(id=id)
+                registra = Registra.objects.get(user=user)
+                registra.delete()
+                user.delete()
+                return redirect(reverse("personel_registra_list"))
+        except user.DoesNotExist:
+            return redirect(reverse("home"))
+        except Registra.DoesNotExist:
+            return redirect(reverse("home"))
+        
+class DeleteProfessorAPI(LoginRequiredMixin, APIView):
+    def get(self, request, id):
+        
+        if not request.user.is_registra:
+            return redirect(reverse("home"))
+
+        try:
+            with transaction.atomic():
+                user = CustomUser.objects.get(id=id)
+                professor = Professor.objects.get(user=user)
+                professor.delete()
+                user.delete()
+                return redirect(reverse("personel_professor_list"))
+        except user.DoesNotExist:
+            return redirect(reverse("home"))
+        except Professor.DoesNotExist:
+            return redirect(reverse("home"))
+        
+class DeleteStudentAPI(LoginRequiredMixin, APIView):
+    def get(self, request, id):
+        
+        if not request.user.is_registra:
+            return redirect(reverse("home"))
+
+        try:
+            with transaction.atomic():
+                user = CustomUser.objects.get(id=id)
+                student = Student.objects.get(user=user)
+                student.delete()
+                user.delete()
+                return redirect(reverse("personel_student_list"))
+        except user.DoesNotExist:
+            return redirect(reverse("home"))
+        except Student.DoesNotExist:
+            return redirect(reverse("home"))
+        
